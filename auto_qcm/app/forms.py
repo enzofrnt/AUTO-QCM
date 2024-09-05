852f85a63e924669667d6eb50df660acbd97a0a2
Fournet Enzo <enzofournet31@gmail.com>
2024-09-05 18:14:40 +0200

    Question creation finished

--- auto_qcm/app/forms.py
+++ auto_qcm/app/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import Question, Reponse
-from django.forms.utils import ErrorDict
+from .models import Question, Reponse, Tag
 
 class QuestionForm(forms.ModelForm):
+    # Champ pour ajouter de nouveaux tags
+    new_tags = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Ajouter des tags séparés par des virgules'}),
+        help_text="Vous pouvez ajouter plusieurs tags séparés par des virgules."
+    )
+
     class Meta:
         model = Question
-        fields = ['texte', 'tags', ]
+        fields = ['texte', 'tags', 'new_tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),  # Affichage des tags existants en tant que checkboxes
+        }
 
 # Formset pour gérer les réponses associées à une question
 ReponseFormSet = inlineformset_factory(
