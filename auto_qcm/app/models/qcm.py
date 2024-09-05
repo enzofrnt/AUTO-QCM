bd03cae62c39dc36879bb8d6b6529024f3f73cb4
November304 <nathan.p3pro@gmail.com>
2024-09-05 11:34:36 +0200

    Rajout fichier test .xml et debut fonction de conversion
    J'ai rajouté un .xml comme modèle a supprimer éventuellement et les fonctions de conversions pour deux des classes à remplir

--- auto_qcm/app/models/qcm.py
+++ auto_qcm/app/models/qcm.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+
 class QCM(models.Model):
     titre = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    questions = models.ManyToManyField('Question', related_name='qcms', blank=True)
+    questions = models.ManyToManyField("Question", related_name="qcms", blank=True)
 
     def __str__(self):
+        return self.titre
+
+    def convertToXml(self):
+
         return self.titre
 
     class Meta:
--- auto_qcm/app/models/question.py
+++ auto_qcm/app/models/question.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 class Question(models.Model):
     texte = models.CharField(max_length=255)
-    tags = models.ManyToManyField('Tag', related_name='questions', blank=True)
+    tags = models.ManyToManyField("Tag", related_name="questions", blank=True)
     number_of_correct_answers = models.IntegerField(
         help_text="Nombre de bonnes réponses possibles. Doit être égal au nombre de réponses marquées comme correctes."
     )
 
     def __str__(self):
+        return self.texte
+
+    def convertToXml(self):
+        # Convertir la question au format xml
         return self.texte
 
     class Meta:
@@ -24,11 +29,12 @@ class Question(models.Model):
                 f"La question doit avoir exactement {self.number_of_correct_answers} bonne(s) réponse(s), "
                 f"mais elle en a {correct_answers_count}."
             )
-        
+
         # Verify thta we have more response than the number of expected correct answers + 1
         if self.reponses.count() < self.number_of_correct_answers + 1:
-            raise ValidationError("La question doit avoir un nombre total de réponse supérieur au nombre de bonne réponse + 1.")
-        
+            raise ValidationError(
+                "La question doit avoir un nombre total de réponse supérieur au nombre de bonne réponse + 1."
+            )
 
     def get_correct_answers(self):
         """Retourne toutes les réponses correctes associées à cette question."""
