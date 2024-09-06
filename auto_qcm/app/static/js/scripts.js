d8e929835e7ac216eaceecf0b299b0885c7e589d
loispacqueteau <loispacqueteau@gmail.com>
2024-09-06 14:35:32 +0200

    Separation du css dans le homepage

--- auto_qcm/app/static/js/scripts.js
+++ auto_qcm/app/static/js/scripts.js
@@ -1,4 +1,17 @@
 function toggleSidebar() {
+    
     document.querySelector('.sidebar').classList.toggle('collapsed');
+    document.querySelector('.toggle-btn').classList.toggle('collapsed');
+    
+    const icon = document.querySelector('.toggle-btn i');
+
+    if (icon.classList.contains('bi-caret-left')) {
+        icon.classList.remove('bi-caret-left');
+        icon.classList.add('bi-caret-right');
+    } else {
+        icon.classList.remove('bi-caret-right');
+        icon.classList.add('bi-caret-left');
+    }
+    
     console.log('toggled');
 }
--- auto_qcm/app/views/home_view.py
+++ auto_qcm/app/views/home_view.py
@@ -3,4 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    # Logique de ton application
+    toggle_active = True  # ou False selon ta logique
+
+    return render(request, 'home.html', {'toggle_active': toggle_active})
