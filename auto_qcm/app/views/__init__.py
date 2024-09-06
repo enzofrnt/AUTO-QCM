1c2a9a3c6175be5a801e7fd6a3cf6f0831cae631
loispacqueteau <loispacqueteau@gmail.com>
2024-09-06 10:01:55 +0200

    Ajout page

--- auto_qcm/app/views/__init__.py
+++ auto_qcm/app/views/__init__.py
@@ -1,3 +1,4 @@
 from .question_list_view import QuestionListView
 from .question_creation_view import QuestionCreateView
 from .remove_tags_view import remove_tag
+from .home_view import home
--- auto_qcm/auto_qcm/urls.py
+++ auto_qcm/auto_qcm/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import QuestionListView, QuestionCreateView, remove_tag
+from app.views import home  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'), 
     path('list-questions/', QuestionListView.as_view(), name='question-list'),
     path('create-questions/', QuestionCreateView.as_view(), name='question-create'),
     path('remove-tag/<int:question_id>/<int:tag_id>/', remove_tag, name='remove-tag'),
+
+
 ]
