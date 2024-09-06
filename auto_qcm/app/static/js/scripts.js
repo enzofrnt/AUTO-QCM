a15c7fd29dddc2f1c60e5889740153ecd6f75e24
loispacqueteau <loispacqueteau@gmail.com>
2024-09-06 16:22:49 +0200

    Theme sombre

--- auto_qcm/app/static/js/scripts.js
+++ auto_qcm/app/static/js/scripts.js
@@ -15,3 +15,44 @@ function toggleSidebar() {
     
     console.log('toggled');
 }
+
+document.getElementById('theme-switch').addEventListener('change', function() {
+    console.log('changed');
+    if (this.checked) {
+      document.body.classList.add('dark-mode');
+      
+      document.body.querySelector('.sidebar').classList.add('dark-mode');
+      listItem=document.body.querySelectorAll('.nav-item');
+      document.body.querySelector('.nav-item').classList.add('dark-mode');
+      console.log("listitem",listItem);
+      
+      listItem.forEach(element => {
+        element.classList.add('dark-mode');
+      });
+
+
+      
+      var listButton = document.body.querySelector('.main-content').querySelectorAll('.button');
+      listButton.forEach(element => {
+        element.classList.add('dark-mode');
+      });
+
+      console.log(listButton);
+      // document.body.querySelector('.navbar').classList.add('dark-mode');
+      console.log('dark mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+      document.body.querySelector('.sidebar').classList.remove('dark-mode');
+      document.body.querySelector('.main-content').querySelector('.button').classList.remove('dark-mode');
+      test = document.body.querySelector('.main-content').querySelectorAll('.button');
+      test.forEach(element => {
+        element.classList.remove('dark-mode');
+      });
+
+      listItem=document.body.querySelectorAll('.nav-item');
+      listItem.forEach(element => {
+        element.classList.remove('dark-mode');
+      }); 
+      
+    }
+  });
