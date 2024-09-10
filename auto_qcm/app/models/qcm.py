c6d09eea5259074f308b594834c855d1a210c9dc
November304 <nathan.p3pro@gmail.com>
2024-09-10 11:39:05 +0200

    Rajout nom d'une question pour l'export moodle

--- auto_qcm/app/models/qcm.py
+++ auto_qcm/app/models/qcm.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class QCM(models.Model):
     titre = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    questions = models.ManyToManyField('Question', related_name='qcms', blank=True)
+    questions = models.ManyToManyField("Question", related_name="qcms", blank=True)
 
     def __str__(self):
         return self.titre
@@ -17,5 +18,4 @@ class QCM(models.Model):
         for quest in self.questions.all():
             texte += quest.convertToXml()
         texte += "</quiz>"
-
         return texte
