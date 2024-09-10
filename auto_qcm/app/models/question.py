9ebd478140574e78149279135f077f2ef442e596
November304 <nathan.p3pro@gmail.com>
2024-09-10 11:39:05 +0200

    Rajout nom d'une question pour l'export moodle

--- auto_qcm/app/models/question.py
+++ auto_qcm/app/models/question.py
@@ -6,26 +6,28 @@ from django.dispatch import receiver
 
 logger = getLogger(__name__)
 
+
 class Question(models.Model):
+    nom = models.CharField(max_length=50, default="QuestionQCM")
     texte = models.CharField(max_length=255)
-    tags = models.ManyToManyField('Tag', related_name='questions', blank=True)
+    tags = models.ManyToManyField("Tag", related_name="questions", blank=True)
 
     def __str__(self):
         return self.texte
 
     class Meta:
         verbose_name = "Question"
-        verbose_name_plural = "Questions"        
+        verbose_name_plural = "Questions"
 
     def get_correct_answers(self):
         """Retourne toutes les réponses correctes associées à cette question."""
         return self.reponses.filter(is_correct=True)
-    
+
     @property
     def number_of_correct_answers(self):
         """Retourne le nombre de réponses correctes associées à cette question."""
         return self.get_correct_answers().count()
-    
+
     def convertToXml(self):
         # Convertir la question au format xml
         texte = (
