c6dc4f823158f906d576d5659c1ef7ca74cac293
Kilianspore <84775788+Kilianspore@users.noreply.github.com>
2024-09-12 09:06:15 +0200

    update dashboard etudiant

--- auto_qcm/app/views/etudiant_dashboard_view.py
+++ auto_qcm/app/views/etudiant_dashboard_view.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
-def etudiant_dashboard(request):
+def etudiant_dashboard(request, pk):
     return render(request, 'dashboard/etudiant_dashboard.html')
