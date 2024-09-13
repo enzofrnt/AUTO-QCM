e7313de197078648903bdd5412285cba79ff1ae3
November304 <nathan.p3pro@gmail.com>
2024-09-13 10:15:44 +0200

    Liste de toutes les questions dans la création
    Quand on crée un qcm on peut choisir les question a mettre dedans

--- auto_qcm/app/views/qcm_create_or_edit_view.py
+++ auto_qcm/app/views/qcm_create_or_edit_view.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from app.models import QCM
-from app.forms import QcmForm, ReponseFormSet
+from app.models import QCM, Question
+from app.forms import QcmForm
 
 @login_required(login_url='login')
 def create_or_edit_qcm(request,pk = None):
     if pk :
         qcm = get_object_or_404(QCM,pk=pk)
+        selected_questions = qcm.questions.all()
     else:
         qcm = QCM()
+        selected_questions = []
     if request.method == 'POST':
         form = QcmForm(request.POST,instance=qcm)
         
@@ -18,9 +20,16 @@ def create_or_edit_qcm(request,pk = None):
             qcm.creator = request.user
             qcm.save()
 
+            selected_questions = request.POST.getlist('selected_questions')
+            qcm.questions.set(selected_questions)
+            qcm.save()
+
             return redirect('qcm-list')
         print(form.errors)
     else:
         form = QcmForm(instance=qcm)
 
-    return render(request, 'qcm/qcm_form.html', {'form': form})
+    #On met tt les questions dans la liste du qcm
+    questions = Question.objects.all()
+
+    return render(request, 'qcm/qcm_form.html', {'form': form,"questions":questions,'selected_questions': selected_questions})
