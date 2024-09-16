fc85b33486b6b9653c61632a4b78dea3e8a3a10f
Kilianspore <84775788+Kilianspore@users.noreply.github.com>
2024-09-16 11:47:41 +0200

    update dashboard + fix panneau a gauche

--- auto_qcm/app/views/etudiant_dashboard_view.py
+++ auto_qcm/app/views/etudiant_dashboard_view.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from app.decorators import teacher_or_student_own_dashboard_required
+from app.models import QCM, ReponseQCM
 
 @login_required(login_url='login')
 @teacher_or_student_own_dashboard_required
 def etudiant_dashboard(request, pk):
-    return render(request, 'dashboard/etudiant_dashboard.html')
+    
+    # Récupérer les informations nécessaires pour le dashboard
+    upcoming_qcms = QCM.objects.filter(date__gte=timezone.now())
+
+    return render(request, 'dashboard/etudiant_dashboard.html', {
+        'upcoming_qcms': upcoming_qcms,
+    })
