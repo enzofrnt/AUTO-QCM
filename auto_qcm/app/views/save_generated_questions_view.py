7588f39e239fd8c09255685979ebd9d3a94e4bd4
Fournet Enzo <enzofournet31@gmail.com>
2024-09-18 17:15:13 +0200

    génération foncitonel

--- auto_qcm/app/views/save_generated_questions_view.py
+++ auto_qcm/app/views/save_generated_questions_view.py
@@ -1,25 +1,41 @@
-from app.models import Question
+import json
+import logging
+
+from app.models import Question, Reponse
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+logger = logging.getLogger(__name__)
 
 
 @login_required
 @csrf_exempt
 def save_generated_questions(request):
     if request.method == "POST":
-        questions = request.POST.getlist("questions[]")
-        user = request.user  # Récupérer l'utilisateur connecté
+        try:
+            # Charger les données JSON du POST
+            questions_data = json.loads(request.body)
+            logger.error(f"Questions générées : {questions_data}")
+            user = request.user  # Récupérer l'utilisateur connecté
 
-        for question_nom in questions:
-            # Ajouter le créateur lors de la création de la question
-            Question.objects.get_or_create(
-                nom=question_nom,
-                defaults={
-                    "creator": user
-                },  # Utiliser l'utilisateur connecté comme créateur
-            )
+            for question_data in questions_data["questions"]:
+                question = Question.objects.create(
+                    texte=question_data["texte"],
+                    nom=question_data["nom"],
+                    creator=user,
+                )
 
-        return JsonResponse({"message": "Questions sauvegardées avec succès !"})
+                for reponse in question_data["reponses"]:
+                    Reponse.objects.create(
+                        texte=reponse["texte"],
+                        is_correct=reponse["is_correct"],
+                        question=question,
+                    )
+
+            return JsonResponse({"message": "Questions sauvegardées avec succès !"})
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON format"}, status=400)
 
     return JsonResponse({"error": "Requête invalide"}, status=400)
