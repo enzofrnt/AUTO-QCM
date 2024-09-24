d72abff7905a9f86ac772ab14e5c3a8f5519c702
AlexiFon <alexi.fonta@outlook.com>
2024-09-24 10:26:18 +0200

    Update Graphiques
    Fonctionne en selectionnant un qcm

--- auto_qcm/app/views/enseignant_dashboard_view.py
+++ auto_qcm/app/views/enseignant_dashboard_view.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.db.models import Count, Q, Avg, F, FloatField
-from django.db.models.functions import Cast
+from django.db.models import Count, Q
 from app.models import ReponseQCM, Question, QCM
 from app.decorators import self_required, teacher_required
 from django.contrib.auth.decorators import login_required
 from datetime import timedelta
+import logging
+
+logger = logging.getLogger(__name__)
 
 @login_required(login_url='login')
 @self_required
@@ -35,16 +37,27 @@ def enseignant_dashboard(request, pk=None):
     for qcm in qcms:
         reponses = qcm.reponses_qcm.all()
         total_responses = reponses.count()
+        
         if total_responses > 0:
-            avg_score = sum(reponse.score for reponse in reponses) / total_responses
+            total_percentage = 0
+            
+            for reponse in reponses:
+                max_score = reponse.score_max if reponse.score_max > 0 else 1  # Éviter la division par zéro
+                percentage = (reponse.score / max_score) * 100
+                total_percentage += percentage
+            
+            avg_score_percentage = total_percentage / total_responses  # Moyenne des pourcentages
         else:
-            avg_score = 0
+            avg_score_percentage = 0
+
         qcms_stats.append({
             'id': qcm.id,
             'titre': qcm.titre,
-            'avg_score': avg_score,
+            'avg_score_percentage': avg_score_percentage,  # Utilisez le pourcentage moyen ici
             'total_responses': total_responses
         })
+
+        logger.error(f"QCM Stats: {qcms_stats}")
 
     context = {
         'enseignant': enseignant,
