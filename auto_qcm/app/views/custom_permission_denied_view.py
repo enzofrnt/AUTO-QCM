10b118ebc58f72993dccdaa3fb25c77530c2221e
AlexiFon <alexi.fonta@outlook.com>
2024-09-25 16:55:20 +0200

    Ajout page erreur 404 et 500

--- auto_qcm/app/views/custom_permission_denied_view.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponseForbidden
-
-def custom_permission_denied_view(request, exception=None):
-    return render(request, 'custompermission/403.html', status=403)
