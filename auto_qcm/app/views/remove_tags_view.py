0a4646904db7f8a9d8dd99ffc82b86ad00ff7491
AlexiFon <alexi.fonta@outlook.com>
2024-09-25 16:08:31 +0200

    Fix login
    Fix login required qui buggé parfois mettant une erreur au lieu de rediriger vers la page de login

--- auto_qcm/app/views/remove_tags_view.py
+++ auto_qcm/app/views/remove_tags_view.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from app.models import Question, Tag
 from app.decorators import teacher_required
+from django.urls import reverse_lazy
 
-@login_required(login_url='login')
+
+@login_required(login_url=reverse_lazy('login'))
 @teacher_required
 def remove_tag(request, question_id, tag_id):
     question = get_object_or_404(Question, id=question_id)
--- auto_qcm/app/views/support_doc_view.py
+++ auto_qcm/app/views/support_doc_view.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-@login_required(login_url='login')
+
+@login_required(login_url=reverse_lazy('login'))
 def support_doc(request):
     return render(request, 'support_doc/support_doc.html')
