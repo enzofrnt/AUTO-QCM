4878b7e2c6c0271a4f5321015a419faf84dc0a76
Kilianspore <84775788+Kilianspore@users.noreply.github.com>
2024-09-26 10:27:02 +0200

    fix-bug et update front

--- auto_qcm/app/views/etudiant_dashboard_view.py
+++ auto_qcm/app/views/etudiant_dashboard_view.py
@@ -6,14 +6,17 @@ from app.decorators import teacher_or_self_student_required
 from app.models import QCM, ReponseQCM, ReponseQuestion
 from django.db.models import Q
 from datetime import timedelta
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @login_required(login_url='login')
 @teacher_or_self_student_required
-def etudiant_dashboard(request, pk):    
-    utilisateur = get_object_or_404(User, pk=pk)
+def etudiant_dashboard(request, pk):   
 
-    
+    utilisateur = get_object_or_404(User, pk=pk)
+        
     # Récupérer les informations nécessaires pour le dashboard    
 
     # Récupérer les réponses au QCM
@@ -26,11 +29,12 @@ def etudiant_dashboard(request, pk):
     upcoming_qcms = QCM.objects.filter(
         Q(date__gte=today) & Q(date__lte=three_months_later)
     ).order_by('date')
-    
 
+    logger.error(utilisateur)
 
 
     return render(request, 'dashboard/etudiant_dashboard.html', {
         'upcoming_qcms': upcoming_qcms,
         'reponse_qcm': reponse_qcm,
+        'utilisateur': utilisateur,
     })
