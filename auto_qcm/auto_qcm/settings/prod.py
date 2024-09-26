1dcd4ec42aca86bde1e90141c56a98ec11fc8748
Fournet Enzo <enzofournet31@gmail.com>
2024-09-26 14:58:19 +0200

    Ajout des logs en prod

--- auto_qcm/auto_qcm/settings/prod.py
+++ auto_qcm/auto_qcm/settings/prod.py
@@ -5,3 +5,24 @@ DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "/static/"
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "/app/log/log.txt",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
