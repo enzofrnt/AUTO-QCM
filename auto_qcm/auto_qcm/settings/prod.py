f9c386cdd7a7838da0cc1ba82a9e737e6cd9127a
Fournet Enzo <enzofournet31@gmail.com>
2024-09-27 10:13:46 +0200

    Mise à jour

--- auto_qcm/auto_qcm/settings/prod.py
+++ auto_qcm/auto_qcm/settings/prod.py
@@ -14,6 +14,9 @@ if DEBUG is False:
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "localhost").split(
+    ","
+)
 
 INSTALLED_APPS.append("django_minify_html")
 
