f4de9c1963e17b6e228d5247f5cc6a8f426b1552
Fournet Enzo <enzofournet31@gmail.com>
2024-10-01 09:28:20 +0200

    fix login

--- auto_qcm/app/views/login_view.py
+++ auto_qcm/app/views/login_view.py
@@ -1,23 +1,25 @@
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
+from logging import getLogger
+
 from django.contrib import messages
 from django.contrib.auth import logout
-from logging import getLogger
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 logger = getLogger(__name__)
 
+
 class CustomLoginView(LoginView):
     form_class = AuthenticationForm
-    template_name = 'login/login.html'
-    success_url = reverse_lazy('home')  # Redirection après login
+    template_name = "login.html"
+    success_url = reverse_lazy("home")  # Redirection après login
 
     def form_valid(self, form):
         """Gérer les actions supplémentaires après un login réussi."""
         # Déconnecter l'utilisateur actuel s'il existe
         if self.request.user.is_authenticated:
             logout(self.request)
-        
+
         # Connexion du nouvel utilisateur
         user = form.get_user()
         logger.info(f"Utilisateur connecté: {user.username}")
@@ -31,7 +33,7 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         """Redirection vers l'URL de succès ou page précédente."""
-        if self.request.GET.get('next'):
-            return self.request.GET.get('next')
+        if self.request.GET.get("next"):
+            return self.request.GET.get("next")
         else:
             return self.success_url
