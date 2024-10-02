1bd57a5702b895e9920df90e676359f4a7224828
November304 <nathan.p3pro@gmail.com>
2024-10-02 17:05:00 +0200

    Remise en vrac des vues
    On attend la fin du sprint pour organiser les vues dans des dossiers, à voir pr plus tard

--- auto_qcm/app/views/__init__.py
+++ auto_qcm/app/views/__init__.py
@@ -5,25 +5,25 @@ from .custom_error_view import (
     custom_permission_denied_view,
     custom_server_error_view,
 )
-from .qcm.delete_multiple_qcms_view import delete_multiple_qcms
-from .dashboard.enseignant_dashboard_view import enseignant_dashboard
-from .dashboard.etudiant_dashboard_view import etudiant_dashboard
+from .delete_multiple_qcms_view import delete_multiple_qcms
+from .enseignant_dashboard_view import enseignant_dashboard
+from .etudiant_dashboard_view import etudiant_dashboard
 from .export_view import export_qcm_latex, export_qcm_xml, export_question_xml
 from .home_view import home
 from .login_view import CustomLoginView
-from .qcm.qcm_answer_view import repondre_qcm
-from .qcm.qcm_correction_view import corriger_qcm
-from .qcm.qcm_create_or_edit_view import create_or_edit_qcm
-from .qcm.qcm_delete_view import delete_qcm
-from .qcm.qcm_list_view import QcmListView
-from .qcm.qcm_acces_view import acces_qcm
-from .dashboard.qcm_statistique_view import qcm_statistics
-from .question.question_create_or_edit_view import create_or_edit_question
-from .question.question_delete_view import delete_question
-from .question.question_generation_view import question_generation_view
-from .question.question_list_view import QuestionListView
+from .qcm_answer_view import repondre_qcm
+from .qcm_correction_view import corriger_qcm
+from .qcm_create_or_edit_view import create_or_edit_qcm
+from .qcm_delete_view import delete_qcm
+from .qcm_list_view import QcmListView
+from .qcm_acces_view import acces_qcm
+from .qcm_statistique_view import qcm_statistics
+from .question_create_or_edit_view import create_or_edit_question
+from .question_delete_view import delete_question
+from .question_generation_view import question_generation_view
+from .question_list_view import QuestionListView
 from .remove_tags_view import remove_tag
-from .dashboard.reponse_qcm_dashboard_view import qcm_responses
-from .question.save_generated_questions_view import save_generated_questions
-from .dashboard.search_students_view import search_student
+from .reponse_qcm_dashboard_view import qcm_responses
+from .save_generated_questions_view import save_generated_questions
+from .search_students_view import search_student
 from .support_doc_view import support_doc
