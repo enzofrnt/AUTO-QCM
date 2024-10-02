b6b8ab2ab27f7c527f5e4784aabb221458c4d422
November304 <nathan.p3pro@gmail.com>
2024-10-02 09:20:39 +0200

    Correction view fonctionnelles ajout filtre sur index
    La correction view avait un problème ou j'utilisais uniquement les reponses donc si on répondait rien on ne voyait rien, j'ai aussi ajouté un filtre de template django qui permet d'acceder à l'index d'une liste

--- auto_qcm/app/views/qcm/qcm_correction_view.py
+++ auto_qcm/app/views/qcm/qcm_correction_view.py
@@ -5,26 +5,31 @@ from app.decorators import teacher_or_self_student_required
 from django.urls import reverse_lazy
 
 
-@login_required(login_url=reverse_lazy('login'))
+@login_required(login_url=reverse_lazy("login"))
 # @teacher_or_self_student_required
 def corriger_qcm(request, repqcm_id):
     reponse_qcm = get_object_or_404(ReponseQCM, id=repqcm_id)
     qcm = reponse_qcm.qcm
     user = reponse_qcm.utilisateur
     reponses_soumis = reponse_qcm.reponses.all()
-    reponses_utilisateur = {}
-
+    scores = []
+    for question in qcm.questions.all():
+        score = "0/" + str(question.note)
+        for repquestion in reponses_soumis:
+            if question == repquestion.question:
+                score = str(repquestion.score) + "/" + str(question.note)
+        scores.append(score)
+    reponsesids = []
     for repquestion in reponses_soumis:
-        if repquestion not in reponses_utilisateur:
-            reponses_utilisateur[repquestion] = []
         for reponse in repquestion.reponse.all():
-            reponses_utilisateur[repquestion].append(reponse.id)
+            reponsesids.append(reponse.id)
 
     context = {
-        'reponse_qcm': reponse_qcm,
-        'qcm': qcm,
-        'reponses_utilisateur': reponses_utilisateur,
-        'user': user
+        "reponse_qcm": reponse_qcm,
+        "qcm": qcm,
+        "scores": scores,
+        "reponsesids": reponsesids,
+        "user": user,
     }
-    
-    return render(request, 'qcm/qcm_correction.html', context)
+
+    return render(request, "qcm/qcm_correction.html", context)
