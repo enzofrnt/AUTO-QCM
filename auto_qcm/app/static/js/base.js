19ae0566cf561d6747a978339ff2e04749a3de57
Fournet Enzo <enzofournet31@gmail.com>
2024-10-04 17:05:50 +0200

    base.js clean

--- auto_qcm/app/static/js/base.js
+++ auto_qcm/app/static/js/base.js
@@ -7,23 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
       // Ajoute un effet lors du passage de la souris
       item.addEventListener('mouseover', function() {
         var theme = localStorage.getItem('theme') || 'light';
-        console.log('mouseover'); // Log pour tester
-        // item.style.backgroundColor = 'rgba(0, 0, 0, 0.1)'; // Fond noirci
-        console.log(theme)
         if (theme === 'dark') {
-            console.log("set to black")
-            // item.style.color = 'rgb(255, 255, 255)';
             item.style.color = 'rgb(0, 0, 0)'; // Texte en noir
 
         } else {
             item.style.color = 'rgb(255, 255, 255)';
-            // item.style.color = 'rgb(0, 0, 0)'; // Texte en noir
         }
       });
 
       // Enlève l'effet quand la souris quitte   l'élément
       item.addEventListener('mouseout', function() {
-        // item.style.backgroundColor = ''; // Réinitialise le fond
         item.style.color = ''; // Réinitialise la couleur du texte
       });
     });
