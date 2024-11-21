1f774bfcb8fd1899745c0b660248cc037f358b07
AlexiFon <AlexiFon@users.noreply.github.com>
2024-11-21 15:31:55 +0000

    pre-commit fixes

--- auto_qcm/app/models/question.py
+++ auto_qcm/app/models/question.py
@@ -1,12 +1,12 @@
-from django.utils import timezone
-from logging import getLogger
 import random
+from logging import getLogger
 
+from app.models.tag import Tag
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from app.models.tag import Tag
+from django.utils import timezone
 
 logger = getLogger(__name__)
 
