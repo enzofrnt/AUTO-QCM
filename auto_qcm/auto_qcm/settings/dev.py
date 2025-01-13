def4cbcf46f58ad37b10fc1d8d1f6577adf501ea
Fournet Enzo <enzofournet31@gmail.com>
2025-01-13 11:49:03 +0100

    base BDD

--- auto_qcm/auto_qcm/settings/dev.py
+++ auto_qcm/auto_qcm/settings/dev.py
@@ -2,3 +2,4 @@ from .base import *
 
 MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
 INSTALLED_APPS.append("django_browser_reload")
+INSTALLED_APPS.append("behave_django")
