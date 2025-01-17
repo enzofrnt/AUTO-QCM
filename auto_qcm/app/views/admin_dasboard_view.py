28ba5be3fd953df17498de6ec00019e533438442
Fournet Enzo <enzofournet31@gmail.com>
2025-01-17 14:53:29 +0100

    admin-dashboard fix

--- auto_qcm/app/views/admin_dasboard_view.py
+++ auto_qcm/app/views/admin_dasboard_view.py
@@ -1,6 +1,7 @@
 from app.decorators import admin_required
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 # Fonction pour vérifier si l'utilisateur est un superutilisateur
@@ -9,6 +10,7 @@ def is_superuser(user):
 
 
 # Vue admin personnalisée
+@login_required(login_url=reverse_lazy("login"))
 @admin_required
 @user_passes_test(is_superuser)
 def custom_admin_view(request):
